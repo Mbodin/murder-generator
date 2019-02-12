16c9f52744bbe0f59c0c735a610b591d652d032f
Martin Bodin <martin.bodin@ens-lyon.org>
2019-02-12 18:02:12 +0000

    Adding a first test on the Travis CI file.

--- build.sh
+++ build.sh
@@ -18,3 +18,8 @@ ocamlbuild -I src \
 # Translate to JavaScript
 js_of_ocaml $TARGET.byte
 
+# Adding license information.
+sed -i "1i/* The source code of this compiled program is available at https://github.com/Mbodin/murder-generator */" $TARGET.js
+sed -i "1i/* @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later */" $TARGET.js
+echo "/* @license-end */" >> $TARGET.js
+
---
+++ src/main.ml
@@ -0,0 +1,6 @@
+(** Module main.
+ * This file is the one compiled to JavaScript, then fetched and executed
+ * to run the whole program. **)
+
+failwith "This is a test.  Please do not report it."
+

1b5f70e3ec18b8a815bd4a4b7a3572d6a9684e29
Martin Bodin <martin.bodin@ens-lyon.org>
2019-02-13 20:09:17 +0000

    Removing the keep-history flag for Travis CI.

--- web/main.js
+++ web/main.js
@@ -1,6 +1,6 @@
 /* @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later */
 
-throw "This is a test.  Please do not report it."
+throw "The “main.js” file has not been compiled: the dummy one was executed instead."
 
 /* @license-end */
 
